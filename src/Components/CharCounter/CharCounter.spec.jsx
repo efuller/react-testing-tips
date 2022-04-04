41d3c7a3c892146364865eaa09d285bda2eb130f
efuller <eric@alley.co>
2022-04-04 19:57:02 -0400

    CharCounter - Test for default text

--- src/Components/CharCounter/CharCounter.spec.jsx
+++ src/Components/CharCounter/CharCounter.spec.jsx
@@ -1,4 +1,11 @@
+import { screen, render } from '@testing-library/react';
+import { CharCounter } from './index';
+
 describe('<CharCounter />', () => {
-	it.todo('should display default text if no character count is passed in');
+	it.only('should display default text if no character count is passed in', () => {
+		render(<CharCounter />);
+
+		expect(screen.getByText(/start typing/i)).toBeInTheDocument();
+	});
 	it.todo('should display character count text if passed in')
 });
--- src/Components/CharCounter/index.jsx
+++ src/Components/CharCounter/index.jsx
@@ -3,7 +3,7 @@ export const CharCounter = () => (
 		<div
 			className="-ml-2 -my-2 rounded-full px-3 py-2 inline-flex items-center text-left text-gray-400 group"
 		>
-			<span className="text-sm text-gray-500 group-hover:text-gray-600 italic">24 characters</span>
+			<span className="text-sm text-gray-500 group-hover:text-gray-600 italic">Start typing...</span>
 		</div>
 	</div>
 );
