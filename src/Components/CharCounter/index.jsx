ace8279241482df1ecc06e98059b7a549ab212a1
efuller <eric@alley.co>
2022-04-04 19:22:25 -0400

    Split out components

---
+++ src/Components/CharCounter/index.jsx
@@ -0,0 +1,9 @@
+export const CharCounter = () => (
+	<div className="flex">
+		<div
+			className="-ml-2 -my-2 rounded-full px-3 py-2 inline-flex items-center text-left text-gray-400 group"
+		>
+			<span className="text-sm text-gray-500 group-hover:text-gray-600 italic">24 characters</span>
+		</div>
+	</div>
+);
