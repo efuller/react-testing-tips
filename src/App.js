0c3e90503435afc346995b0462c1906341d130f7
efuller <eric@alley.co>
2022-04-05 08:31:02 -0400

    Update logo

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ function App() {
     <>
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-2xl">
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-            alt="Workflow"
-          />
+          <svg xmlns="http://www.w3.org/2000/svg"   className="mx-auto h-12 w-12 w-auto stroke-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+          </svg>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create Posts</h2>
         </div>
 
