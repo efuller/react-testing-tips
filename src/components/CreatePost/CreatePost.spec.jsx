d80370d36dc7e95f3586344e9cd5e592266b91c6
efuller <eric@alley.co>
2022-04-04 21:17:48 -0400

    Show an error when submitting form without a title

--- src/components/CreatePost/CreatePost.spec.jsx
+++ src/components/CreatePost/CreatePost.spec.jsx
@@ -1,5 +1,17 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import { CreatePost } from './index';
+
 describe('<CreatePost />', () => {
-	it.todo('should display an error if the form is submitted with no title');
+	it.only('should display an error if the form is submitted with no title', async () => {
+		render(<CreatePost />);
+
+		const button = screen.getByRole('button');
+		fireEvent.submit(button);
+
+		const alert = await screen.findAllByRole('alert');
+		expect(alert.length).toBe(1);
+		expect(alert[0]).toHaveTextContent(/title/i);
+	});
 	it.todo('should hide error once title has been entered')
 	it.todo('should display the content character count text after entering content');
 	it.todo('should call createPost with the correct correct data');
--- src/components/CreatePost/index.jsx
+++ src/components/CreatePost/index.jsx
@@ -1,54 +1,72 @@
+import React from 'react';
 import { CharCounter } from '../CharCounter';
 
-export const CreatePost = () => (
-	<form action="#" className="relative">
-		<div className="border border-gray-300 mb-5 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
-			<label htmlFor="title" className="sr-only">
-				Post Title
-			</label>
-			<input
-				type="text"
-				name="title"
-				id="title"
-				className="block w-full border-0 pt-2.5 text-lg font-medium placeholder-gray-500 focus:ring-0"
-				placeholder="Title"
-			/>
-			<label htmlFor="content" className="sr-only">
-				Post Content
-			</label>
-			<textarea
-				rows={2}
-				name="content"
-				id="content"
-				className="block w-full border-0 py-0 resize-none placeholder-gray-500 focus:ring-0 sm:text-sm"
-				placeholder="Write post content here..."
-				defaultValue={''}
-			/>
+export const CreatePost = () => {
+	const [title, setTitle] = React.useState('');
+	const [content, setContent] = React.useState('');
+	const [formError, setFormError] = React.useState(false);
 
-			{/* Spacer element to match the height of the toolbar */}
-			<div aria-hidden="true">
-				<div className="py-2">
-					<div className="h-9" />
-				</div>
-				<div className="h-px" />
-				<div className="py-2">
-					<div className="py-px">
+	const handleOnSubmit = () => {
+		if (!title) {
+			setFormError(true);
+			return;
+		}
+	}
+
+	return (
+		<form className="relative" onSubmit={handleOnSubmit}>
+			<div className="border border-gray-300 mb-5 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+				<label htmlFor="title" className="sr-only">
+					Post Title
+				</label>
+				<input
+					type="text"
+					name="title"
+					id="title"
+					className="block w-full border-0 pt-2.5 text-lg font-medium placeholder-gray-500 focus:ring-0"
+					placeholder="Title"
+				/>
+				<label htmlFor="content" className="sr-only">
+					Post Content
+				</label>
+				<textarea
+					rows={2}
+					name="content"
+					id="content"
+					className="block w-full border-0 py-0 resize-none placeholder-gray-500 focus:ring-0 sm:text-sm"
+					placeholder="Write post content here..."
+					defaultValue={''}
+				/>
+
+				{/* Spacer element to match the height of the toolbar */}
+				<div aria-hidden="true">
+					<div className="py-2">
 						<div className="h-9" />
+					</div>
+					<div className="h-px" />
+					<div className="py-2">
+						<div className="py-px">
+							<div className="h-9" />
+						</div>
+					</div>
+				</div>
+				<div className="border-t border-gray-200 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
+					<CharCounter />
+					<div className="flex-shrink-0">
+						<button
+							type="submit"
+							className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						>
+							Create
+						</button>
 					</div>
 				</div>
 			</div>
-			<div className="border-t border-gray-200 px-2 py-2 flex justify-between items-center space-x-3 sm:px-3">
-				<CharCounter />
-				<div className="flex-shrink-0">
-					<button
-						type="submit"
-						className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>
-						Create
-					</button>
-				</div>
-			</div>
-		</div>
-		<div role="alert" className="text-red-700"><span>* Please enter a post title.</span></div>
-	</form>
-);
+			{
+				formError ? (
+					<div role="alert" className="text-red-700">* Please enter a post title.</div>
+				) : null
+			}
+		</form>
+		)
+};
