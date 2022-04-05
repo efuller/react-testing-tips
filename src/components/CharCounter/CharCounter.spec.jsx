8666096abd9da41224907116d5a5ad4a2cc7349a
efuller <eric@alley.co>
2022-04-05 09:28:26 -0400

    More test comments

--- src/components/CharCounter/CharCounter.spec.jsx
+++ src/components/CharCounter/CharCounter.spec.jsx
@@ -5,12 +5,14 @@ describe('<CharCounter />', () => {
 	it('should display default text if no character count is passed in', () => {
 		render(<CharCounter />);
 
+		// Assert that the default character count text is displaying.
 		expect(screen.getByText(/start typing/i)).toBeInTheDocument();
 	});
 
 	it('should display character count text if passed in', () => {
 		render(<CharCounter characterCount={32} />);
 
+		// Assert that the character count text is displaying.
 		expect(screen.getByText(/32 characters/i)).toBeVisible();
 	});
 });
