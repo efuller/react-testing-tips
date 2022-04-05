9b59e1f8b17713ac5aaf4ce4e1c15bf7c339f439
efuller <eric@alley.co>
2022-04-04 20:05:30 -0400

    CharCounter - test for characterCount prop

--- src/Components/CharCounter/CharCounter.spec.jsx
+++ src/Components/CharCounter/CharCounter.spec.jsx
@@ -2,10 +2,15 @@ import { screen, render } from '@testing-library/react';
 import { CharCounter } from './index';
 
 describe('<CharCounter />', () => {
-	it.only('should display default text if no character count is passed in', () => {
+	it('should display default text if no character count is passed in', () => {
 		render(<CharCounter />);
 
 		expect(screen.getByText(/start typing/i)).toBeInTheDocument();
 	});
-	it.todo('should display character count text if passed in')
+
+	it('should display character count text if passed in', () => {
+		render(<CharCounter characterCount={32} />);
+
+		expect(screen.getByText(/32 characters/i)).toBeVisible();
+	});
 });
--- src/Components/CharCounter/index.jsx
+++ src/Components/CharCounter/index.jsx
@@ -1,9 +1,25 @@
-export const CharCounter = () => (
+import PropTypes from 'prop-types';
+
+export const CharCounter = ( { characterCount }) => (
 	<div className="flex">
 		<div
 			className="-ml-2 -my-2 rounded-full px-3 py-2 inline-flex items-center text-left text-gray-400 group"
 		>
-			<span className="text-sm text-gray-500 group-hover:text-gray-600 italic">Start typing...</span>
+			<span className="text-sm text-gray-500 group-hover:text-gray-600 italic">
+				{
+					characterCount ?
+						`${characterCount} characters` :
+						'Start typing...'
+				}
+			</span>
 		</div>
 	</div>
 );
+
+CharCounter.defaultProps = {
+	characterCount: 0,
+}
+
+CharCounter.propTypes = {
+	characterCount: PropTypes.number.isRequired,
+};
