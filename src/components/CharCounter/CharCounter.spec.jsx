13c21f810162ade41cd4149d90dfd54d24b05231
efuller <eric@alley.co>
2022-04-05 09:45:24 -0400

    Test todos only

--- src/components/CharCounter/CharCounter.spec.jsx
+++ src/components/CharCounter/CharCounter.spec.jsx
@@ -1,18 +1,4 @@
-import { screen, render } from '@testing-library/react';
-import { CharCounter } from './index';
-
 describe('<CharCounter />', () => {
-	it('should display default text if no character count is passed in', () => {
-		render(<CharCounter />);
-
-		// Assert that the default character count text is displaying.
-		expect(screen.getByText(/start typing/i)).toBeInTheDocument();
-	});
-
-	it('should display character count text if passed in', () => {
-		render(<CharCounter characterCount={32} />);
-
-		// Assert that the character count text is displaying.
-		expect(screen.getByText(/32 characters/i)).toBeVisible();
-	});
+	it.todo('should display default text if no character count is passed in');
+	it.todo('should display character count text if passed in');
 });
