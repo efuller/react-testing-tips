bb02eadc5857961a45c2dd3a730990d715131ed3
efuller <eric@alley.co>
2022-04-04 20:40:12 -0400

    CreatePost - test todos

---
+++ src/components/CreatePost/CreatePost.spec.jsx
@@ -0,0 +1,6 @@
+describe('<CreatePost />', () => {
+	it.todo('should display an error if the form is submitted with no title');
+	it.todo('should display the content character count text after entering content');
+	it.todo('should call createPost with the correct correct data');
+	it.todo('should disable create button while submitting new post');
+})
